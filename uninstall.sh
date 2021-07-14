6cd3d5e7e0da6f3e68bdd2e6c6c77bbb18b82e12
Alex <alexbrucely@gmail.com>
2021-07-14 11:02:52 -0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -31,8 +31,6 @@ kubectl --context ${CONTEXT} delete ns istio-operator
 # delete CRDs
 # gloo-mesh
 kubectl --context ${CONTEXT} delete crd accesslogrecords.observability.enterprise.mesh.gloo.solo.io accesspolicies.networking.mesh.gloo.solo.io certificaterequests.certificates.mesh.gloo.solo.io destinations.discovery.mesh.gloo.solo.io issuedcertificates.certificates.mesh.gloo.solo.io kubernetesclusters.multicluster.solo.io meshes.discovery.mesh.gloo.solo.io podbouncedirectives.certificates.mesh.gloo.solo.io rolebindings.rbac.enterprise.mesh.gloo.solo.io roles.rbac.enterprise.mesh.gloo.solo.io settings.settings.mesh.gloo.solo.io trafficpolicies.networking.mesh.gloo.solo.io virtualdestinations.networking.enterprise.mesh.gloo.solo.io virtualmeshes.networking.mesh.gloo.solo.io wasmdeployments.networking.enterprise.mesh.gloo.solo.io workloads.discovery.mesh.gloo.solo.io xdsconfigs.xds.agent.enterprise.mesh.gloo.solo.io
-# argocd
-kubectl --context ${CONTEXT} delete crd applications.argoproj.io appprojects.argoproj.io
 # istio
 kubectl --context ${CONTEXT} delete crd authorizationpolicies.security.istio.io destinationrules.networking.istio.io envoyfilters.networking.istio.io gateways.networking.istio.io istiooperators.install.istio.io peerauthentications.security.istio.io requestauthentications.security.istio.io serviceentries.networking.istio.io sidecars.networking.istio.io virtualservices.networking.istio.io workloadentries.networking.istio.io workloadgroups.networking.istio.io
 # gloo-edge
