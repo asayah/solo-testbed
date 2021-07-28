2f770bf6b48ed0329429b0bcaa6b0510a3b21b2d
Alex <alexbrucely@gmail.com>
2021-07-28 16:38:58 -0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -49,4 +49,5 @@ kubectl --context ${CONTEXT} delete crd accesslogrecords.observability.enterpris
 kubectl --context ${CONTEXT} delete crd authorizationpolicies.security.istio.io destinationrules.networking.istio.io envoyfilters.networking.istio.io gateways.networking.istio.io istiooperators.install.istio.io peerauthentications.security.istio.io requestauthentications.security.istio.io serviceentries.networking.istio.io sidecars.networking.istio.io virtualservices.networking.istio.io workloadentries.networking.istio.io workloadgroups.networking.istio.io
 # gloo-edge
 kubectl --context ${CONTEXT} delete crd proxies.gloo.solo.io settings.gloo.solo.io upstreamgroups.gloo.solo.io upstreams.gloo.solo.io authconfigs.enterprise.gloo.solo.io gateways.gateway.solo.io ratelimitconfigs.ratelimit.solo.io routetables.gateway.solo.io virtualservices.gateway.solo.io 
-
+# knative
+kubectl --context ${CONTEXT} delete crd certificates.networking.internal.knative.dev clusterdomainclaims.networking.internal.knative.dev configurations.serving.knative.dev domainmappings.serving.knative.dev images.caching.internal.knative.dev ingresses.networking.internal.knative.dev metrics.autoscaling.internal.knative.dev podautoscalers.autoscaling.internal.knative.dev revisions.serving.knative.dev routes.serving.knative.dev serverlessservices.networking.internal.knative.dev services.serving.knative.dev 
