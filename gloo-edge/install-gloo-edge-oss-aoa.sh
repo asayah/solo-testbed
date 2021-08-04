b5d4610cd3507e5ae1305171e2777bf3d03cc9f9
Alex <alexbrucely@gmail.com>
2021-08-04 11:03:23 -0700

    restructure to oss script

---
+++ gloo-edge/install-gloo-edge-oss-aoa.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CONTEXT=$1
+
+### check to make sure that arguments were set before taking off
+if [[ ${CONTEXT} == "" ]]
+then
+  echo "Missing arguments. Proper usage is ./install-script.sh <context>"
+  echo "example:"
+  echo "./install-argocd.sh mgmt"
+  echo "would install argocd on the mgmt context"
+  exit 1
+else
+  echo "Beginning install on context ${CONTEXT}...."
+fi
+
+# use context
+kubectl config use-context ${CONTEXT}
+
+# deploy gloo-edge-oss app-of-apps
+kubectl --context ${CONTEXT} create -f https://raw.githubusercontent.com/ably77/solo-testbed-apps/main/argo-apps/environments/gloo-edge-oss/edge/meta/meta-gloo-edge.yaml
+
+### check gloo-edge-oss deployment status
+../tools/wait-for-rollout.sh deployment gateway gloo-system 10
+
+# deploy frontend app-of-apps
+kubectl --context ${CONTEXT} create -f https://raw.githubusercontent.com/ably77/solo-testbed-apps/main/argo-apps/environments/gloo-edge-oss/frontend/meta/meta-frontend-apps.yaml
+
+# deploy virtualservices app-of-apps
+kubectl --context ${CONTEXT} create -f https://raw.githubusercontent.com/ably77/solo-testbed-apps/main/argo-apps/environments/gloo-edge-oss/virtualservice/meta/meta-virtualservices.yaml
